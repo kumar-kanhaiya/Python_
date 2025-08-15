e2475ebf6a8b949e6625f3c0ef5e55f84e1ccf71
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-15 19:16:38 +0530

    code

---
+++ Chapter-5/Problem_set.py
@@ -0,0 +1,28 @@
+# Question - 1
+
+translator = {"rat" : "चूहा" , 
+              "bat" : "बल्ला",
+              "table" : "मेज़"}
+
+print(translator , type(translator))
+
+set = set({});
+a = int(input("Enter your number : "))
+set.add(a);
+b = int(input("Enter your number : "))
+set.add(b);
+c = int(input("Enter your number : "))
+set.add(c)
+d = int(input("Enter your number : "))
+set.add(d);
+e = int(input("Enter your number : "))
+set.add(e);
+print(set)
+
+# Qestion - 3
+
+# "yes"
+
+# question - 4
+
+
---
+++ Chapter-5/SetMethod.py
@@ -0,0 +1,25 @@
+
+s = {1,5,32,54,5};
+""""
+Method
+s.add
+clear
+copy
+s.len()
+s.remove(value)
+s.pop() remove an arbitrary element from the
+        set and return the element removed \
+s.union()
+s.intersection() // comman part  
+
+"""
+
+"""
+Properties 
+set are unordered 
+sets are unindexed 
+there is no way to change items in sets 
+sets cannot contain duplicate value 
+"""
+s.add(566)
+print(s)
