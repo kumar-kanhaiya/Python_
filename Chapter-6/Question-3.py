303f94127562bdcc66986f67154447248228016c
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-15 19:47:56 +0530

    code

---
+++ Chapter-6/Question-3.py
@@ -0,0 +1,8 @@
+username = input("Enter your username greater than 10 length : ")
+
+if(len(username) < 10):
+    print("Your username is less than 10 length ")
+    print("Try something new ")
+
+else:
+    print("This is valid username ")
