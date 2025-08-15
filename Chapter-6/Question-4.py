56dacd1b9563535169c9ec57c4b917a685eec813
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-15 19:50:57 +0530

    code

---
+++ Chapter-6/Question-4.py
@@ -0,0 +1,10 @@
+list = ["kanhaiya", "keshav" , "shubham" , "harsh" , "manish"]
+
+count = 0;
+name = input("Enter your name for check ")
+
+if(list.count(name) > 0):
+    print("it is present ")
+
+else:
+    print("it is not present ")
