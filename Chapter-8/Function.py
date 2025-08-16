db3946e650a8d91bfca481355e7c169adea0b182
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-16 22:10:03 +0530

    code'

--- Chapter-8/Function.py
+++ Chapter-8/Function.py
@@ -1,1 +1,8 @@
 #  fuction 
+#  function is a group of statement performing a specific type 
+#  syntex of function 
+def average(a,b,c):
+    av = (a+b+c)/3;
+    return av;
+
+print(average(5,6,7))
