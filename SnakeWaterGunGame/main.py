23d86576b6eabe1cde076f04f7efbf1c5859d4f0
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-17 11:14:16 +0530

    code

--- SnakeWaterGunGame/main.py
+++ SnakeWaterGunGame/main.py
@@ -1,35 +1,37 @@
-
+import random
 """
 1 for snake 
 -1 for water 
 0 for gun 
 """
 
-computer = -1;
+computer = random.choice([-1,0,1]);
 youstr =  input("Enter your choice: ")
 youDict = {"s":1 , "w" : -1 , "g":0};
 you = youDict[youstr];
 
 if(computer == you):
     print("It's Draw")
-elif(computer ==-1 and you ==1):
-    print("You Win !")
-
-elif(computer ==-1 and you ==0):
-    print("You Lose")
-
-elif(computer ==1 and you ==-1):
-    print("You Lose")
-
-elif(computer ==1 and you ==0):
-    print("You Win !")
-
-
-elif(computer ==0 and you ==-1):
-    print("You Win !")
-
-elif(computer ==0 and you ==0):
-    print("You Lose")
 
 else:
-    print("Something went wrong")
+    if(computer ==-1 and you ==1):
+        print("You Win !")
+
+    elif(computer ==-1 and you ==0):
+        print("You Lose")
+
+    elif(computer ==1 and you ==-1):
+        print("You Lose")
+
+    elif(computer ==1 and you ==0):
+        print("You Win !")
+
+
+    elif(computer ==0 and you ==-1):
+        print("You Win !")
+
+    elif(computer ==0 and you ==0):
+        print("You Lose")
+
+    else:
+        print("Something went wrong")
