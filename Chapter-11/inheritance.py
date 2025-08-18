b5f849b4862f3ae66732de86c727a69ef89c945a
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-18 06:28:03 +0530

    code

--- Chapter-11/inheritance.py
+++ Chapter-11/inheritance.py
@@ -7,4 +7,9 @@ class Employee:
 #  use of inheritance 
 
 class programmer(Employee):
+    company = "itc infotech "
+    def show(self):
+        print(f"the name is {self.name} and the salary is {self.salary}")
 
+
+# child inherit all properties of parent 
