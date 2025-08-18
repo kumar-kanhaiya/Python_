9e15a065fae547efcc6a0c8474c638ff7f9bd952
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-18 06:18:04 +0530

    code

--- Chapter-10/self.py
+++ Chapter-10/self.py
@@ -5,6 +5,11 @@ class Employee:
     def getInfo(self):
         print(f"The language is {self.language}. The salary is {self.salary}")
 
+    @staticmethod
+    def great():
+        print("Good morning")    
+
 kanhaiya = Employee();
-kanhaiya.getInfo()        
+kanhaiya.getInfo()
+kanhaiya.great()        
 
