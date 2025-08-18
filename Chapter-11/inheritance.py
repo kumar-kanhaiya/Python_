431a9ee1ad14b2d59dbe0f6c9de2072a415ed15f
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-18 06:26:32 +0530

    code

---
+++ Chapter-11/inheritance.py
@@ -0,0 +1,10 @@
+class Employee:
+    company = "itc"
+    def show(self):
+        print(f"the name is {self.name} and the salary is {self.salary}")
+
+
+#  use of inheritance 
+
+class programmer(Employee):
+
